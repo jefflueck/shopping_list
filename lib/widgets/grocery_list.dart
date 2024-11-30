4ccb9b447690fb171fdd99c71a9143445dec7046
jefflueck <lueckcfd@yahoo.com>
2024-11-29 20:03:37 -0600

    added flutter http package, hooked all list functions to live firebase database, all front-end to back-end working as expected.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,21 +15,85 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https('shopping-list-24da1-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (element) => element.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    if (newItem == null) {
+    _loadItems();
+  }
+
+  void _deleteItem(String id) async {
+    final url = Uri.https('shopping-list-24da1-default-rtdb.firebaseio.com',
+        'shopping-list/$id.json');
+    await http.delete(url);
+    _loadItems();
+  }
+
+  void _clearList() async {
+    final url = Uri.https('shopping-list-24da1-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    await http.delete(url);
+    _loadItems();
+  }
+
+  void _undoItemDeletion(GroceryItem item) async {
+    final url = Uri.https('shopping-list-24da1-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+
+    final response = await http.put(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'name': item.name,
+        'quantity': item.quantity,
+        'category': item.category.title,
+      }),
+    );
+
+    if (response.statusCode != 200) {
+      // Handle error
       return;
     }
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+
+    _loadItems(); // Refresh the list
   }
 
   @override
@@ -83,11 +151,13 @@ class _GroceryListState extends State<GroceryList> {
                             setState(() {
                               _groceryItems.insert(index, deletedItem);
                             });
+                            _undoItemDeletion(deletedItem);
                           },
                         ),
                       ),
                     );
                   });
+                  _deleteItem(deletedItem.id);
                 },
                 child: ListTile(
                   title: Text(_groceryItems[index].name),
@@ -111,6 +181,7 @@ class _GroceryListState extends State<GroceryList> {
               onPressed: () {
                 setState(() {
                   _groceryItems.clear();
+                  _clearList();
                 });
               },
             ),
